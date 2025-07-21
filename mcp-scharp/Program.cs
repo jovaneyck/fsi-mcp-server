baed572d22ba4a36558b0b548f3e297b8722d134
Jo Van Eyck <jovaneyck1988@hotmail.com>
2025-07-21 18:51:24 +0200

    add c# mcp server

---
+++ mcp-scharp/Program.cs
@@ -0,0 +1,34 @@
+﻿//How to test: https://github.com/modelcontextprotocol/inspector
+//npx @modelcontextprotocol/inspector dotnet run
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"Hello from C#: {message}";
+
+    [McpServerTool, Description("Echoes in reverse the message sent by the client.")]
+    public static string ReverseEcho(string message) => new string(message.Reverse().ToArray());
+}
