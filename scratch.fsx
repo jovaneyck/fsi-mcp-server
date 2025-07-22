66665cc7d4fe497a60866bac63e9d2b18be54b12
Jo Van Eyck <jovaneyck1988@hotmail.com>
2025-07-22 20:35:15 +0200

    cleanup scratch file

--- scratch.fsx
+++ scratch.fsx
@@ -1,10 +1,3 @@
 // F# Interactive Scratch File
 
 let word = "strawberry"
-
-let countChar (char : char) (str : string) : int = 
-    str 
-    |> Seq.filter (fun c -> c = char) 
-    |> Seq.length
-
-let countRs = countChar 'r' word
